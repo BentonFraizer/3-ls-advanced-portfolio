0ad49bd0d10903530175ce0f94b1624b17603334
Yuri Lopuhin <FOL2203012@yandex.ru>
2020-08-16 22:51:19 +0900

    Нет контента. На проверку Еремееву

---
+++ src/scripts/works.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+
+const thumbs = {
+  template: "#preview-thumbs"
+};
+
+const btns = {
+  template: "#preview-btns"
+};
+
+const display = {
+  template: "#preview-display",
+  components: {thumbs, btns},
+};
+
+const tags = {
+  template: "#preview-tags"
+};
+
+const info = {
+  template: "#preview-info",
+  components: {tags},
+};
+
+new Vue ({
+  el: "#preview-component",
+  template: "#preview-container",
+  components: {display, info},
+});
